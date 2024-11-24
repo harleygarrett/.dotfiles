aca2c6e7e425cb0b054fdcb6ba74cf00d303ba0d
Harley Garrett <harleygarrett@protonmail.com>
2024-11-24 16:12:22 +0000

    Extract vscode-specific symlinks to own directory

--- symlinks.sh
+++ symlinks.sh
@@ -1,5 +1,2 @@
 # .zshrc
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
-
-# Visual Studio Code
-ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
---
+++ vscode/symlinks.sh
@@ -0,0 +1,2 @@
+# User settings
+ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
