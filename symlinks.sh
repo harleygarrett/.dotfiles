e6c16488f46029326f2814947008454938ff1e36
Harley Garrett <harleygarrett@protonmail.com>
2024-11-24 16:09:33 +0000

    Reorder symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,5 +1,5 @@
-# Visual Studio Code
-ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-
 # .zshrc
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+
+# Visual Studio Code
+ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
