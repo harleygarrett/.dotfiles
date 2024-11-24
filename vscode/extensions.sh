5e326f581a0cdc450c245df3e25cda8eb4383021
Harley Garrett <harleygarrett@protonmail.com>
2024-11-24 12:59:33 +0000

    Install basic vscode extensions

---
+++ vscode/extensions.sh
@@ -0,0 +1,14 @@
+# Code Time -- link to GitHub account
+code --install-extension softwaredotcom.swdc-vscode
+# Todo Tree
+code --install-extension gruntfuggly.todo-tree
+# Prettier
+code --install-extension esbenp.prettier-vscode
+# Duplicate action
+code --install-extension mrmlnc.vscode-duplicate
+# px to rem
+code --install-extension cipchk.cssrem
+# hex-to-rgba
+code --install-extension dakshmiglani.hex-to-rgba
+# Stylelint
+code --install-extension stylelint.vscode-stylelint
