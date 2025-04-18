dfcfc791f38e3ee176146f944b562e067042cfb4
Harley Garrett <70719407+harleygarrett@users.noreply.github.com>
2025-04-18 15:02:20 +0100

    Update VSCode settings and extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -12,3 +12,7 @@ code --install-extension cipchk.cssrem
 code --install-extension dakshmiglani.hex-to-rgba
 # Stylelint
 code --install-extension stylelint.vscode-stylelint
+# PHP Sniffer
+code --install-extension wongjn.php-sniffer
+# Tailwind Intellisense
+code --install-extension bradlc.vscode-tailwindcss
